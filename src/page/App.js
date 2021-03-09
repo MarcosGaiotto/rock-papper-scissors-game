0437388e4fc51cb144d87e523fa647de6c333ea9
MarcosGaiotto <marcosgaiotto@hotmail.com>
2021-03-08 22:09:09 -0300

    Painel final adicionado

--- src/page/App.js
+++ src/page/App.js
@@ -8,7 +8,8 @@ function App() {
   const [selectedElement, setSelectedElement] = useState(null);
   const [hasComputerSelected, setHasComputerSelected] = useState(false);
   const [selectedElementByComputer, setSelectedElementByComputer] = useState("not-selected");
-  const [result, setResult] = useState(null);
+  const [result, setResult] = useState("The House is choosing...");
+  const [hasAResult, setHasAResult] = useState(false);
 
   useEffect(() => {
     const elements = document.querySelectorAll('.element');
@@ -18,67 +19,85 @@ function App() {
         setHasElementSelected(true);
       })
     })
-  }, [])
+  }, [resetGame])
 
   useEffect  (() => {
-    if(!hasComputerSelected) {
-      setHasComputerSelected(true);
-      setTimeout( async () =>{
+    if(!hasComputerSelected && hasElementSelected) {
+      setTimeout(() =>{
           const elementOptions = ["rock", "paper", "scissors"]
           const elementSorted = elementOptions[Math.round(Math.random() * (elementOptions.length - 1))]
-          await setSelectedElementByComputer(elementSorted);
-      }, 5000)
-    
+          setSelectedElementByComputer(elementSorted);
+          setHasComputerSelected(true);
+      }, 3000)
     }
-  }, [setSelectedElement]);
+  }, [hasElementSelected]);
 
   useEffect(() => {
-    if(selectedElement === selectedElementByComputer){
-      setResult("DRAW");
-    } else if(selectedElement === "paper") {
-      if (selectedElementByComputer === "rock") {
-        setResult("YOU WIN");
+    if(hasComputerSelected && hasElementSelected) {
+      if(selectedElement === selectedElementByComputer){
+        setResult("DRAW");
+      } else if(selectedElement === "paper") {
+        if (selectedElementByComputer === "rock") {
+          setResult("YOU WIN");
+        } else {
+          setResult("YOU LOSE");
+        }
+      } else if(selectedElement === "rock") {
+        if (selectedElementByComputer === "scissors") {
+          setResult("YOU WIN");
+        } else {
+          setResult("YOU LOSE");
+        }
       } else {
-        setResult("YOU LOSE");
+        if (selectedElementByComputer === "paper") {
+          setResult("YOU WIN");
+        } else {
+          setResult("YOU LOSE");
+        }
       }
-    } else if(selectedElement === "rock") {
-      if (selectedElementByComputer === "scissors") {
-        setResult("YOU WIN");
-      } else {
-        setResult("YOU LOSE");
-      }
-    } else {
-      if (selectedElementByComputer === "paper") {
-        setResult("YOU WIN");
-      } else {
-        setResult("YOU LOSE");
-      }
+      setHasAResult(true);
     }
-  }, [selectedElementByComputer])
-  
+  }, [hasComputerSelected])
+
+  function resetGame() {
+    setHasElementSelected(false);
+    setHasComputerSelected(false);
+    setSelectedElement(null);
+    setSelectedElementByComputer("not-selected");
+    setResult("The House is choosing...");
+    setHasAResult(false);
+  }
   
   return (
     <div className="App">
       {hasElementSelected ? (
         <div className="painel-final">
-          <Element classElement="element" type={selectedElement}/>
-          {hasComputerSelected ? (
-            <div className="final-result">
-              <p>{ result }</p>
-              <Element classElement="element" type={selectedElementByComputer}/>
-            </div>
-          ) : (
-            <Element />
-          )}
-          
+          <div className="element-chosen">
+            <p>YOU PICKED</p>
+            <Element classElement="element" type={selectedElement}/>
+          </div>
+          <div className="final-result">
+            <p className="result-text">{ result }</p>
+            { hasAResult && (
+               <button className="reset-button" onClick={resetGame}>Play Again</button>
+            )}
+          </div> 
+          <div className="element-chosen">
+            <p>THE HOUSE PICKED</p>
+            <Element classElement="element" type={selectedElementByComputer}/>
+          </div>
         </div>
       ) : (
         <div className="painel">
           <Element classElement="element" type="paper"/>
-          <Element classElement="element down" type="rock"/>
           <Element classElement="element" type="scissors"/>
+          <Element classElement="element down" type="rock"/>
         </div>
       )}
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Marcos Gaiotto</a>.
+      </div>
     </div>
   );
 }
